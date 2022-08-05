db83483c0c606217298af5fdfa8fd124876ec121
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-08-05 12:26:25 -0300

    Primeiro projeto do ignite.

---
+++ primeiro-projeto/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import './global.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World!!</h1>
+    </div>
+  )
+}
+
+export default App
