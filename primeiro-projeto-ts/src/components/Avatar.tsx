e66bc21229818466f2e01fd0fdb55e85aafa8a2a
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-08-08 01:41:09 -0300

    Extends html element props

--- primeiro-projeto-ts/src/components/Avatar.tsx
+++ primeiro-projeto-ts/src/components/Avatar.tsx
@@ -1,15 +1,16 @@
 import styles from './Avatar.module.css';
+import { ImgHTMLAttributes } from 'react'
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder: boolean;
-  src: string;
-  alt?: string;
 }
 
-const Avatar = ({hasBorder, src, alt}: AvatarProps) => {
+const Avatar = ({hasBorder, ...props}: AvatarProps) => {
   return (
     <img
-    className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src} alt={alt} />
+    className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+    {...props}
+    />
   )
 }
 export default Avatar
