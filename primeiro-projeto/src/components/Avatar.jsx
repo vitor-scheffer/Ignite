f942c4fbd9a610c6f5ac2993a3bf18126439c25b
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-08-08 01:59:51 -0300

    Excluindo projeto em jsx.

--- primeiro-projeto/src/components/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Avatar.module.css';
-
-const Avatar = ({hasBorder, src}) => {
-  return (
-    <img
-    className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src} />
-  )
-}
-export default Avatar
